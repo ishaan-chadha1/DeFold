4792e1c5c0fec4836df6c75687bf9a63f340fd9e
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-09-22 02:27:15 +0800

    Revert Back to Original to Hopefully Fix Deployment WorldCoin Issue

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,6 @@ export default {
       {
         source: "/api/noun",
         destination: "https://noun-api.com/beta/pfp", // Proxy to external API
-      },
-      {
-        source: "/api/worldcoin",
-        destination: "https://bridge.worldcoin.org/request", // Proxy to Worldcoin API
       }
     ];
   },
